ccfeb76868c0acc18f1e529cb7f96f07a0b06aa6
xuzhaoju <15265855235@139.com>
2021-08-11 17:34:13 +0800

    全员测试

--- gmall-coupon/src/main/java/com/xzj/gmall/gmallcoupon/GamllCouponApplication.java
+++ gmall-coupon/src/main/java/com/xzj/gmall/gmallcoupon/GamllCouponApplication.java
@@ -1,9 +1,11 @@
 package com.xzj.gmall.gmallcoupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xzj.gmall.gmallcoupon.dao")
 public class GamllCouponApplication {
 
     public static void main(String[] args) {
--- gmall-member/src/main/java/com/xzj/gmall/gmallmember/GmallMemberApplication.java
+++ gmall-member/src/main/java/com/xzj/gmall/gmallmember/GmallMemberApplication.java
@@ -1,9 +1,11 @@
 package com.xzj.gmall.gmallmember;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xzj.gmall.gmallmember.dao")
 public class GmallMemberApplication {
 
     public static void main(String[] args) {
