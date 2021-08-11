48bee674934da723d950d6ba7aa7250178faa71f
xuzhaoju <15265855235@139.com>
2021-08-11 22:20:46 +0800

    使用nacos作为注册中心>添加配置在yml中&添加springcloudeAlibaba和nacos依赖

--- gmall-member/src/main/java/com/xzj/gmall/gmallmember/GmallMemberApplication.java
+++ gmall-member/src/main/java/com/xzj/gmall/gmallmember/GmallMemberApplication.java
@@ -4,6 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
+
+
 @SpringBootApplication
 @MapperScan("com.xzj.gmall.gmallmember.dao")
 public class GmallMemberApplication {
