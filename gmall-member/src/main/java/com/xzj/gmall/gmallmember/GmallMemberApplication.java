d6ff9c7979190b8902c07b4a25bf5029bfe643fc
xuzhaoju <15265855235@139.com>
2021-08-15 17:45:52 +0800

    创建gateway

--- gmall-member/src/main/java/com/xzj/gmall/gmallmember/GmallMemberApplication.java
+++ gmall-member/src/main/java/com/xzj/gmall/gmallmember/GmallMemberApplication.java
@@ -3,6 +3,7 @@ package com.xzj.gmall.gmallmember;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,7 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  *  3）启动远程调用
  *      注解：在启动类上添加@EnableFeignClients("com.xzj.gmall.gmallmember.feign")
  */
-
+@EnableDiscoveryClient
 @EnableFeignClients("com.xzj.gmall.gmallmember.feign")
 @SpringBootApplication
 @MapperScan("com.xzj.gmall.gmallmember.dao")
