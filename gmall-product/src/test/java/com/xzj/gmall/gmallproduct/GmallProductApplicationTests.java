6a49cdafaf2e86897ca72d20fb4b17ce64358591
xuzhaoju <15265855235@139.com>
2021-09-02 17:20:01 +0800

    oss上传图片

--- gmall-product/src/test/java/com/xzj/gmall/gmallproduct/GmallProductApplicationTests.java
+++ gmall-product/src/test/java/com/xzj/gmall/gmallproduct/GmallProductApplicationTests.java
@@ -1,20 +1,31 @@
 package com.xzj.gmall.gmallproduct;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.xzj.gmall.gmallproduct.entity.BrandEntity;
 import com.xzj.gmall.gmallproduct.service.BrandService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GmallProductApplicationTests {
 
 
     @Autowired
-    private BrandService brandService;
+    BrandService brandService;
+    @Autowired
+    OSSClient client;
     /**
      * @describe:增加
      *
@@ -95,5 +106,39 @@ class GmallProductApplicationTests {
             System.out.println(brandEntity);
         }
     }
+    @Test
+    public void update() throws FileNotFoundException {
+        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tDqZoQ7Gv655PLfjArr";
+        String accessKeySecret = "tGUS5FHrQBwYEARIhycXAM1n6v2piC";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("D:\\bg.jpg");
+        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("shoping-storage-server", "images/bg.png", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+    }
+
+
+    @Test
+    public void updateByAlibaba() throws FileNotFoundException {
+
+        // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("D:\\bg.jpg");
+        // 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        client.putObject("shoping-storage-server", "images/bg.png", inputStream);
+
+        // 关闭OSSClient。
+        client.shutdown();
+    }
+
+
 
 }
